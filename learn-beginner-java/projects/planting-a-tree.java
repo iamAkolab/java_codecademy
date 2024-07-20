42e584b1d3e1b3fc6e7ac79fbbc91774ead700ec
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2024-07-19 21:59:06 -0600

    Create planting-a-tree.java

---
+++ learn-beginner-java/projects/planting-a-tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+class Tree {
+  public static void main(String[] args) {
+    // Print out greeting
+    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+    System.out.println("    **  ");
+    System.out.println("    **  ");
+    System.out.println("   **** ");
+    System.out.println("  ******  ");
+    System.out.println("  * ** *  ");
+  }
+}
