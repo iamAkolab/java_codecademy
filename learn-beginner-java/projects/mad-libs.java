785713d12e12a0362c7d2c25df00ecd7e928f306
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2024-07-20 00:04:51 -0600

    Create mad-libs.java

---
+++ learn-beginner-java/projects/mad-libs.java
@@ -0,0 +1,35 @@
+public class MadLibs {
+  /*
+  Your description here
+  */
+  	public static void main(String[] args){
+
+      /*
+      This program generates a mad lib story.
+      Author: Laura
+      Date: 2/19/2049
+      */
+      
+      String name1 = "Voilet";
+      String adjective1 = "good";
+      String adjective2 = "great";
+      String adjective3 = "funny";
+      String verb1 = "dance";
+      String noun1 = "women";
+      String noun2 = "apples";
+      String noun3 = "music";
+      String noun4 = "dogs";
+      String noun5 = "robots";
+      String noun6 = "ice cream";
+      String name2 = "zeus";
+      int number = 2050;
+      String place1 = "New York";
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
+    }       
+}
+
+// This morning Voilet woke up feeling good. 'It is going to be a great day!' Outside, a bunch of womens were protesting to keep apples in stores. They began to dance to the rhythm of the music, which made all the dogss very funny. Concerned, Voilet texted zeus, who flew Voilet to New York and dropped Voilet in a puddle of frozen robots. Voilet woke up in the year 2050, in a world where ice creams ruled the world.
