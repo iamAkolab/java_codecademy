fb08baa3c6199c0bbe6d39495de4c13fe75ed549
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2024-08-08 23:22:04 -0600

    Update basic-calculator.java

--- learn-beginner-java/projects/basic-calculator.java
+++ learn-beginner-java/projects/basic-calculator.java
@@ -8,3 +8,38 @@ In this project, you will use classes, methods, and objects to create a simple a
 - Divide two integers
 - Apply the modulo operator on two integers
 */
+
+public class Calculator{
+  public static void main(String[] args) {
+    // Basic Calculator that adds, subtratcs, multiply and divide integers
+    Calculator myCalculator = new Calculator();
+
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+  }
+
+  public Calculator() {
+
+  }
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+
+   public double divide(int a, int b) {
+    return a / b;
+  }
+
+  public int modulo(int a, int b) {
+    return a % b;
+  }
+
+}
