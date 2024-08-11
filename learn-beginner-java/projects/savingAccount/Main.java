ce0fb474a084acaf6c10d4d248cf0ac37e3251ae
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2024-08-11 01:05:42 -0600

    Create Main.java

---
+++ learn-beginner-java/projects/savingAccount/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+  public static void main(String[] args) {
+    SavingsAccount savings = new SavingsAccount(2000);
+        
+    //Check balance:
+    savings.checkBalance();
+        
+    //Withdrawing:
+    savings.withdraw(300);
+        
+    //Check balance:
+    savings.checkBalance();
+        
+    //Deposit:
+    savings.deposit(600);
+        
+    //Check balance:
+    savings.checkBalance();
+        
+    //Deposit:
+    savings.deposit(600);
+        
+    //Check balance:
+    savings.checkBalance();
+        
+    System.out.println(savings);
+  }
+}
