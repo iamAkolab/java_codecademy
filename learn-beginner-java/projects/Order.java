0c3c9579a405a7054765ba4b82e2f98b98effb81
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-01-05 00:06:59 -0700

    Update Order.java

--- learn-beginner-java/projects/Order.java
+++ learn-beginner-java/projects/Order.java
@@ -1,3 +1,57 @@
+// If-Else
+public class Order {
+  boolean isFilled;
+  double billAmount;
+  String shipping;
+  String couponCode;
+  
+  public Order(boolean filled, double cost, String shippingMethod, String coupon) {
+		if (cost > 24.00) {
+      System.out.println("High value item!");
+    }
+    isFilled = filled;
+    billAmount = cost;
+    shipping = shippingMethod;
+    couponCode = coupon;
+  }
+  
+  public void ship() {
+    if (isFilled) {
+      System.out.println("Shipping");
+      System.out.println("Shipping cost: " + calculateShipping());
+    } else {
+      System.out.println("Order not ready");
+    }
+  }
+  
+  public double calculateShipping() {
+    if (shipping.equals("Regular")) {
+      return 0;
+    } else if (shipping.equals("Express")) {
+      // Add your code here
+      if (couponCode == "ship50"){
+        return .85;
+      } else {
+        return 1.75;
+      }
+
+      
+    } else {
+      return .50;
+    }
+ 	}
+  
+  public static void main(String[] args) {
+    // do not alter the main method!
+    Order book = new Order(true, 9.99, "Express", "ship50");
+    Order chemistrySet = new Order(false, 72.50, "Regular", "freeShipping");
+    
+    book.ship();
+    chemistrySet.ship();
+  }
+}
+
+// Switch statement
 public class Order {
   boolean isFilled;
   double billAmount;
@@ -22,14 +76,20 @@ public class Order {
   }
   
   public double calculateShipping() {
-	 	// declare conditional statement here
-    if(shipping == "Regular"){
-      return 0;
-    } else if(shipping == "Express"){
-      return 1.75;
-    } else {
-      return 0.5;
+    double shippingCost;
+	 	// declare switch statement here
+    switch (shipping) {
+      case "Regular":
+       shippingCost = 0;
+	break;
+      case "Express":
+       shippingCost = 1.75;
+	break;
+      default:
+       shippingCost = 0.5;
     }
+    
+    return shippingCost;
  	}
   
   public static void main(String[] args) {
