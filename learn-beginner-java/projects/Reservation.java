d2d39c8b2e3a43bd97878e6e7de8045db897e4a4
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-01-30 22:16:07 -0700

    Update Reservation.java

--- learn-beginner-java/projects/Reservation.java
+++ learn-beginner-java/projects/Reservation.java
@@ -4,7 +4,8 @@ public class Reservation {
   int restaurantCapacity;
   boolean isRestaurantOpen;
   boolean isConfirmed;
-  
+
+	
   public Reservation(int count, int capacity, boolean open) {
     if (count < 1 || count > 8) {
       System.out.println("Invalid reservation!");
