c84a61482c05fa5beb02acb9178dfa27e4183d26
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-03-11 22:26:00 -0600

    Create Coffee.java

---
+++ learn-beginner-java/projects/Coffee.java
@@ -0,0 +1,23 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    int cupsOfCoffee = 1;
+    
+    while (cupsOfCoffee <= 100) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      cupsOfCoffee++;
+      
+    }
+
+    for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+
+          // code that will run
+          System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      }
+
+    
+  }
+  
+}
