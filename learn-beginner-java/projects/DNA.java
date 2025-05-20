78a0a8b5d03531717f50058d20e44a4bd8089a6a
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-05-20 00:29:50 -0600

    Update DNA.java

--- learn-beginner-java/projects/DNA.java
+++ learn-beginner-java/projects/DNA.java
@@ -24,3 +24,48 @@ ATGCGATACTGA is a protein because it has the start codon ATG, the stop codon TGA
 ATGCGATAGA is not a protein because the sequence length is not divisible by 3, so the third condition is not satisfied.
 
 */
+
+public class DNA {
+  //program that determines whether there is a protein in a strand of DNA.
+  
+  public static void main(String[] args) {
+             
+    //  -. .-.   .-. .-.   .
+    //    \   \ /   \   \ / 
+    //   / \   \   / \   \  
+    //  ~   `-~ `-`   `-~ `-
+
+    String dna1 = "ATGCGATACGCTTGA";
+    String dna2 = "ATGCGATACGTGA";
+    String dna3 = "ATTAATATGTACTGA";
+
+    String[] dnaStrands = {dna1,dna2,dna3};
+    //System.out.println(dna.length());
+
+    for (String dna : dnaStrands) {
+          // existing code
+
+        // Find start codon: 
+        int start = dna.indexOf("ATG");
+        System.out.println("Start: " + start);
+
+        // Find stop codon:
+        int stop = dna.indexOf("TGA");
+        System.out.println("Stop: " + stop);
+        if (start != -1 &&
+        stop != -1 &&
+        (stop - start) % 3 == 0) {
+          
+        String protein = dna.substring(start, stop+3); 
+        System.out.println("Protein: " + protein);
+      
+        } else {
+        
+        System.out.println("No protein.");
+    
+         }
+
+      }
+  
+    }
+}
