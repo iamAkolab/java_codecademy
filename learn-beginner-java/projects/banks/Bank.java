a873eeeec1c0267cf846c2984b92137197564f8a
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-05-25 01:23:01 -0600

    Update Bank.java

--- learn-beginner-java/projects/banks/Bank.java
+++ learn-beginner-java/projects/banks/Bank.java
@@ -1,10 +1,18 @@
 public class Bank{
+    private CheckingAccount accountOne;
+  private CheckingAccount accountTwo;
+  
+  public Bank(){
+    accountOne = new CheckingAccount("Zeus", 100, "1");
+    accountTwo = new CheckingAccount("Hades", 200, "2");
+    myAccount = new CheckingAccount("Mike", 300, "3");
+  }
+  
   public static void main(String[] args){
-    CheckingAccount accountOne = new CheckingAccount("Zeus", 100, "1");
-    CheckingAccount accountTwo = new CheckingAccount("Hades", 200, "2");
-    CheckingAccount myAccount = new CheckingAccount("Mike", 300, "3");
-    System.out.println(accountOne.getBalance());
-    accountOne.setBalance(5000);
-    System.out.println(accountOne.getBalance());
+    Bank bankOfGods = new Bank();
+    System.out.println(bankOfGods.accountOne.getBalance());
+    bankOfGods.accountOne.setBalance(5000);
+    System.out.println(bankOfGods.accountOne.getBalance());
+    System.out.println(bankOfGods.accountOne.getMonthlyInterest());
   }
 }
