9bde029407b46c253f690c9e9b2dbba78c60b1b1
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 01:03:32 -0600

    Create Main.java

---
+++ learn-beginner-java/projects/ChildParentClass/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String[] args) {
+    Dinner noodlesDinner = new Dinner();
+    Noodle biangBiang = new BiangBiang();
+
+    noodlesDinner.makeNoodles(biangBiang, "soy sauce and chili oil");
+  }
+}
