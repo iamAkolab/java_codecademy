52a26fd0fe003c3ad74bfd51ace7a0db8ea7aeb8
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 01:09:12 -0600

    Update Ramen.java

--- learn-beginner-java/projects/InheritanceConstructor/Ramen.java
+++ learn-beginner-java/projects/InheritanceConstructor/Ramen.java
@@ -2,7 +2,13 @@ public class Ramen extends Noodle {
   Ramen() {
     super(30.0, 0.3, "flat", "wheat flour");  
   }
+  
   public boolean isTasty(){
     return false;
+  }
+
+    @Override
+  public String getCookPrep() {
+    return "Boil ramen for 5 minutes in broth, then add meat, mushrooms, egg, and vegetables.";
   }
 }
