8445a5ad065ba605552ef08a6c3fa3ddf41b9b55
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 00:34:12 -0600

    Update Main.java

--- learn-beginner-java/projects/InheritanceConstructor/Main.java
+++ learn-beginner-java/projects/InheritanceConstructor/Main.java
@@ -2,5 +2,9 @@ public class Main{
   public static void main(String[] args) {
       Pho phoChay = new Pho();
       System.out.println(phoChay.shape);
+
+    Ramen yasaiRamen = new Ramen();
+    //System.out.println(yasaiRamen.ingredients);
+    System.out.println(yasaiRamen.isTasty());  
   }
 }
