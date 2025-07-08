fd755672567e0acc085c3d80cd3219c0a9a1258b
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 00:52:12 -0600

    Update Spaetzle.java

--- learn-beginner-java/projects/InheritanceConstructor/Spaetzle.java
+++ learn-beginner-java/projects/InheritanceConstructor/Spaetzle.java
@@ -4,4 +4,10 @@ class Spaetzle extends Noodle {
     this.texture = "lumpy and liquid";     
   }
     // Add the new cook() method below:  
+  @Override
+  public void cook(){
+    System.out.println("Grinding or scraping the dough into the pot of boiling water.");
+    texture = "cooked";
+    System.out.println("Boiling");
+  }
 }
