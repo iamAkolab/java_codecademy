7582d28dd07a499fb1e69b0469a0a42b426948c5
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 01:13:28 -0600

    Update Pho.java

--- learn-beginner-java/projects/InheritanceConstructor/Pho.java
+++ learn-beginner-java/projects/InheritanceConstructor/Pho.java
@@ -2,4 +2,9 @@ class Pho extends Noodle {
   public Pho(){
     super(30.0, 0.64, "flat", "rice flour");
   }
+
+   @Override
+  public String getCookPrep() {
+    return "Soak pho for 1 hour, then boil for 1 minute in broth. Then garnish with cilantro and jalapeno.";
+  }
 }
