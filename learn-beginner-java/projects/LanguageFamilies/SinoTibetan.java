300b3aa7a2c440b54e15f799b5d3a34b2f6dbab7
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 01:46:36 -0600

    Update SinoTibetan.java

--- learn-beginner-java/projects/LanguageFamilies/SinoTibetan.java
+++ learn-beginner-java/projects/LanguageFamilies/SinoTibetan.java
@@ -4,6 +4,7 @@ public class SinoTibetan extends Language{
         super(name, numSpeakers, "Asia", "Subject | Object | Verb");
         if (name.contains("Chinese")) {
             this.wordOrder = "Subject | Vers | Object";
+             // this.wordOrder = "subject-verb-object";
         }
     }
 }
