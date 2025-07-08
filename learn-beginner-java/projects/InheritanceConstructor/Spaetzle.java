6607d4fca41255a0a1313d9428ae3d4d1a1b797c
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-08 00:46:01 -0600

    Create Spaetzle.java

---
+++ learn-beginner-java/projects/InheritanceConstructor/Spaetzle.java
@@ -0,0 +1,7 @@
+class Spaetzle extends Noodle {
+  Spaetzle() {
+    super(3.0, 1.5, "irregular", "eggs, flour, salt");
+    this.texture = "lumpy and liquid";     
+  }
+    // Add the new cook() method below:  
+}
