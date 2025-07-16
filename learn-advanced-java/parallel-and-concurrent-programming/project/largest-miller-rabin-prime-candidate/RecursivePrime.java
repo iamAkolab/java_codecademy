c5e2fccb5000be415f342d0e13d2ce198fa10e46
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-16 00:08:02 -0600

    Create RecursivePrime.java

---
+++ learn-advanced-java/parallel-and-concurrent-programming/project/largest-miller-rabin-prime-candidate/RecursivePrime.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RecursivePrime extends RecursiveAction {
+  private BigInteger n;
+  private int k;
+
+  public RecursivePrime(BigInteger n, int k) {
+    this.n = n;
+    this.k = k;
+  }
+
+  
+}
