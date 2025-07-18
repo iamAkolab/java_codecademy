6c9cda4afcd43fd1e49503bd865027002db5c0b0
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-17 23:48:20 -0600

    Update Main.java

--- learn-intermediate-java/projects/GetTakeOut/Main.java
+++ learn-intermediate-java/projects/GetTakeOut/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    
+      // Step 1: Get user input for name and money
+  // Step 2: Create Customer
+  // Step 3: Create TakeOutSimulator
+  // Step 4: Start simulation
   }    
 }
