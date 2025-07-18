4bdcf399945bd36c3c2c7847e5a4e075f628a82c
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-07-18 01:27:29 -0600

    Create Gym.java

---
+++ learn-intermediate-java/projects/ThreadingInTheGym/Gym.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Gym {
+    private final int totalGymMembers;
+    private Map<MachineType, Integer> availableMachines;
+
+    public Gym(int totalGymMembers, Map<MachineType, Integer> availableMachines) {
+        this.totalGymMembers = totalGymMembers;
+        this.availableMachines = availableMachines;
+    }
+
+    public void openForTheDay() {
+        List<Thread> gymMembersRoutines;
+        // Create a thread for each gym member's routine
+        gymMembersRoutines = IntStream.rangeClosed(1, this.totalGymMembers).mapToObj((id) -> {
+            Member member = new Member(id);
+            return new Thread(() -> {
+                try {
+                    member.performRoutine();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            });
+        }).collect(Collectors.toList());
+        Thread supervisor = createSupervisor(gymMembersRoutines);
+        gymMembersRoutines.forEach(Thread::start);
+        supervisor.start();
+    }
+
+    private Thread createSupervisor(List<Thread> threads) {
+        Thread supervisor = new Thread(() -> {
+            while (true) {
+                List<String> runningThreads = threads.stream().filter((t) -> t.isAlive()).map((t) -> t.getName())
+                        .collect(Collectors.toList());
+                System.out.println(Thread.currentThread().getName() + " - " + runningThreads.size()
+                        + " members currently exercising: " + runningThreads + "\n");
+                if (runningThreads.isEmpty()) {
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + ", all threads completed.");
+        });
+        supervisor.setName("Gym Staff");
+        return supervisor;
+    }
+
+    public static void main(String[] args) {
+        Gym sobesGym = new Gym(5, new HashMap<>() {
+            {
+                put(MachineType.LEGPRESSMACHINE, 5);
+                put(MachineType.BARBELL, 5);
+                put(MachineType.SQUATMACHINE, 5);
+                put(MachineType.LEGEXTENSIONMACHINE, 5);
+                put(MachineType.LEGCURLMACHINE, 5);
+                put(MachineType.LATPULLDOWNMACHINE, 5);
+                put(MachineType.CABLECROSSOVERMACHINE, 5);
+            }
+        });
+
+        sobesGym.openForTheDay();
+    }
+}
