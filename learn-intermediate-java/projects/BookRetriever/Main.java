8d7a78cc2b094c9e5ca9696600e44895dff26335
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-11-03 01:33:14 -0700

    Update Main.java

--- learn-intermediate-java/projects/BookRetriever/Main.java
+++ learn-intermediate-java/projects/BookRetriever/Main.java
@@ -3,10 +3,8 @@ public class Main {
     int myNumber = 24;
     String bookName = "Hello Book!";
     // Enter your code below...
-
     Retriever<Integer> containerRetriever = new Container<>(myNumber);
     Retriever<String> bookRetriever = new Book(bookName);
-
     System.out.println("Container retrieved data: "+ containerRetriever.retrieveData());
     System.out.println("Book retrieved data: " + bookRetriever.retrieveData());
   }
