927e07e0391b1550629e0447af316e3707fb5557
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-06 23:01:34 -0700

    Adjust word order for Chinese in SinoTibetan class

--- learn-beginner-java/projects/LanguageFamilies/SinoTibetan.java
+++ learn-beginner-java/projects/LanguageFamilies/SinoTibetan.java
@@ -2,9 +2,11 @@ public class SinoTibetan extends Language {
 
     SinoTibetan(String name, int numSpeakers) {
         super(name, numSpeakers, "Asia", "Subject | Object | Verb");
+        
         if (name.contains("Chinese")) {
             this.wordOrder = "Subject | Vers | Object";
              // this.wordOrder = "subject-verb-object";
         }
+        
     }
 }
