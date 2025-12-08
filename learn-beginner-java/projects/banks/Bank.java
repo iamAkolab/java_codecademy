da8a156628ddae09bba5fbd8084ab33895fc9b32
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-07 22:18:31 -0700

    Fix formatting in Bank constructor

--- learn-beginner-java/projects/banks/Bank.java
+++ learn-beginner-java/projects/banks/Bank.java
@@ -3,7 +3,7 @@ public class Bank {
   private CheckingAccount accountOne;
   private CheckingAccount accountTwo;
   
-  public Bank(){
+  public Bank() {
     accountOne = new CheckingAccount("Zeus", 100, "1");
     accountTwo = new CheckingAccount("Hades", 200, "2");
     myAccount = new CheckingAccount("Mike", 300, "3");
