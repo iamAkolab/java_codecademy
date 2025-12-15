7ec90ba2042f1f702b1466be1008698fd778971e
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-14 17:57:38 -0700

    Fix string formatting in planting-a-tree.java

--- learn-beginner-java/projects/planting-a-tree.java
+++ learn-beginner-java/projects/planting-a-tree.java
@@ -4,10 +4,12 @@ class Tree {
     // Print out greeting
     System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
     System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+    
     System.out.println("    **    ");
     System.out.println("    **    ");
     System.out.println("   ****   ");
     System.out.println("  ******  ");
     System.out.println("  * ** *  ");
+    
   }
 }
