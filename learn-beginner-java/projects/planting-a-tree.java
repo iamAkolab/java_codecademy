f07e902d236d2d085527124a5e290dd5b2d867ec
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-15 17:06:13 -0700

    Add greeting messages in the Tree class
    Added greeting messages to the main method.

--- learn-beginner-java/projects/planting-a-tree.java
+++ learn-beginner-java/projects/planting-a-tree.java
@@ -1,6 +1,7 @@
 // Define your tree class in this file!
 class Tree {
   public static void main(String[] args) {
+    
     // Print out greeting
     System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
     System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
