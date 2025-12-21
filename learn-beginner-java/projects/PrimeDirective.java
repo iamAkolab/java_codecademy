c607c347e2cd446f3bccb8c19ca9c85a216817bf
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-20 19:43:56 -0700

    Add documentation for PrimeDirective class
    Added a comment block explaining the Prime Directive and prime numbers.

--- learn-beginner-java/projects/PrimeDirective.java
+++ learn-beginner-java/projects/PrimeDirective.java
@@ -6,9 +6,21 @@ import java.util.ArrayList;
 
 
 class PrimeDirective {
+
+    /*
+    The Prime Directive
+    Finding prime numbers is a common technical challenge in programming interviews.
+    
+    As it happens, Java loops are a great tool to help you do this!
+    
+    A prime number is an integer greater than 1 that is only divisible by 1 and itself.
+    
+    For example, 2, 3, 5, and 7 are all prime numbers, but 4, 6, 8, and 9 are not.
+    
+    Your prime directive: Build a PrimeDirective.java program that creates an ArrayList of all prime numbers in an array.
+  */
   
   // Add your methods here:
-  
   
   public static void main(String[] args) {
 
