6af6999037e5f5860df1f17675e5df8ae1a8d96c
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-22 00:03:02 -0700

    Update TicTacToe.java

--- learn-beginner-java/projects/TicTacToe.java
+++ learn-beginner-java/projects/TicTacToe.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		// Declare a 2d array of float values called floatTwoD
+	// Declare a 2d array of float values called floatTwoD
     float[][] floatTwoD;
 		floatTwoD = new float[4][10];
 		
