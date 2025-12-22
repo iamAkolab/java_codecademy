a16efba742ec2ab968b9082f6a33404d823f74dd
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2025-12-22 00:03:25 -0700

    Update TicTacToe.java

--- learn-beginner-java/projects/TicTacToe.java
+++ learn-beginner-java/projects/TicTacToe.java
@@ -1,12 +1,13 @@
 public class Main {
 	public static void main(String[] args) {
-	// Declare a 2d array of float values called floatTwoD
-    float[][] floatTwoD;
+		
+		// Declare a 2d array of float values called floatTwoD
+    	float[][] floatTwoD;
 		floatTwoD = new float[4][10];
 		
-    char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
+    	char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
 		
-    ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
+        ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
 
     }
 }
