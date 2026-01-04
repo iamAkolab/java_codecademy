df00945ad9faf4c11b1fd38cfb86321542c45c63
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2026-01-03 23:40:40 -0700

    Enhance makeNoodles method to include sauce details

--- learn-beginner-java/projects/ChildParentClass/Dinner.java
+++ learn-beginner-java/projects/ChildParentClass/Dinner.java
@@ -1,6 +1,8 @@
 class Dinner {
   public void makeNoodles(Noodle noodle, String sauce) {
+    
     noodle.cook();
+    
     System.out.println("Mixing " + noodle.texture + " noodles made from " + noodle.ingredients + " with " + sauce + ".");
     System.out.println("Dinner is served!");
   }  
