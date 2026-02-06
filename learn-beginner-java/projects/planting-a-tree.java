dcf06f6a246eeb4b78dbb3756bb687db4d9fe8fc
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2026-02-05 23:08:33 -0700

    Modify greeting in planting-a-tree.java
    Updated greeting message to include an extra exclamation mark.

--- learn-beginner-java/projects/planting-a-tree.java
+++ learn-beginner-java/projects/planting-a-tree.java
@@ -5,7 +5,7 @@ class Tree {
   public static void main(String[] args) {
     
     // Print out greeting
-    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+    System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!!");
     System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
     
     System.out.println("    **    ");
